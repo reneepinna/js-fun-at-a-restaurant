c7783268dab0c51f5a7051860d956a50a7c2855f
Renee Pinna <renee.pinna@gmail.com>
2023-05-12 09:56:51 -0600

    add functionality to removeMenuItem and checkForFood

--- src/restaurant.js
+++ src/restaurant.js
@@ -8,21 +8,46 @@ function createRestaurant(name){
 }
 
 function addMenuItem(restaurant, item){
-  //determine which menu to add the item
-    if(item.type === "breakfast"){
-      restaurant.menus.breakfast.push(item);
-    } else if(item.type === "lunch"){
-      restaurant.menus.lunch.push(item);
-    }else{
-      restaurant.menus.dinner.push(item);
+  
+  for(var i = 0; i < restaurant.menus[item.type].length; i++){
+    if(restaurant.menus[item.type][i] === item){
+      return restaurant;
     }
-  //return the resturant
+  }
+  restaurant.menus[item.type].push(item);
   return restaurant;
+}
+
+
+function removeMenuItem(resturant, itemName, type){
+  //If the array is empty return right away
+  if(resturant.menus[type].length === 0){
+    return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
+  }
+
+  //If the array has items, look for the requested item
+  for(var i = 0; i < resturant.menus[type].length; i++){
+    if(resturant.menus[type][i].name === itemName){
+      resturant.menus[type].splice( i, 1);
+      return `No one is eating our ${itemName} - it has been removed from the ${type} menu!`;
+    }else {
+      return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
+    }
+  } 
+}
+
+function checkForFood(restaurant, item){
+  for(var i = 0; i < restaurant.menus[item.type].length; i++){
+    if(restaurant.menus[item.type][i] === item){
+      return `Yes, we're serving ${item.name} today!`;
+    }
+  }
+  return `Sorry, we aren't serving ${item.name} today.`;
 }
 
 module.exports = {
   createRestaurant, 
   addMenuItem,
-  // removeMenuItem,
-  // checkForFood
+  removeMenuItem,
+  checkForFood
 }
